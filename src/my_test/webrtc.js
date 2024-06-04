81902a59bd1ce1d2f889589820d4201616f4c9bb
hwachin.lee <fireman11650@gmail.com>
2024-06-04 10:42:47 +0900

    test inversed resolution

---
+++ src/my_test/webrtc.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebRTC 720x1280 Resolution Test</title>
+</head>
+<body>
+    <video id="localVideo" autoplay playsinline></video>
+    <script src="webrtc.js"></script>
+</body>
+</html>
+
