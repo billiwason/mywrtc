db7966bd6dfa449d5d73578b50213663f22a7070
billiwason <96734631+billiwason@users.noreply.github.com>
2024-06-13 17:10:58 +0900

    Update webrtc.js 360 640

--- src/my_test/webrtc.js
+++ src/my_test/webrtc.js
@@ -2,8 +2,8 @@ async function startWebRTC() {
     // 사용자 정의 해상도 설정 (720x1280)
     const constraints = {
         video: {
-            width: { ideal: 480 },
-            height: { ideal: 854 }
+            width: { ideal: 360 },
+            height: { ideal: 640 }
         }
     };
 
